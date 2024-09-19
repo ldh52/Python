433a311befd13e4eb675cdb2578afb841909c5b4
Donghyun Lee <166485055+ldh52@users.noreply.github.com>
2024-09-19 11:04:55 +0900

    Delete multiply_three_numbers.py

--- multiply_three_numbers.py
+++
@@ -1,8 +0,0 @@
-def multiply_three_numbers(num_1, num_2, num_3):
-    print(num_1 * num_2 * num_3)
-
-
-# 테스트 코드
-multiply_three_numbers(7, 3, 5)
-multiply_three_numbers(21, 4, 9)
-multiply_three_numbers(-7, 6, 3)
