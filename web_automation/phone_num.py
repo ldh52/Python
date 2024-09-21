9c661fd2c26388d4fdc65774763e52dbc44f14d7
Donghyun Lee <166485055+ldh52@users.noreply.github.com>
2024-09-21 13:07:21 +0900

    Update phone_num.py

--- web_automation/phone_num.py
+++ web_automation/phone_num.py
@@ -1,13 +1,17 @@
 import requests
 from bs4 import BeautifulSoup
+from openpyxl import Workbook
+
+# 워크북 생성
+wb = Workbook(write_only=True)
+ws = wb.create_sheet()
+ws.append(['지점 이름', '주소', '전화번호'])
 
 # HTML 코드 받아오기
 response = requests.get("https://workey.codeit.kr/orangebottle/index")
 
 # BeautifulSoup 사용해서 HTML 코드 정리
 soup = BeautifulSoup(response.text, 'html.parser')
-
-branch_infos = []
 
 # 모든 지점에 대한 태그 가져오기
 branch_tags = soup.select('div.branch')
@@ -17,7 +21,6 @@ for branch_tag in branch_tags:
     branch_name = branch_tag.select_one('p.city').get_text()
     address = branch_tag.select_one('p.address').get_text()
     phone_number = branch_tag.select_one('span.phoneNum').get_text()
-    branch_infos.append([branch_name, address, phone_number])
+    ws.append([branch_name, address, phone_number])
 
-# 테스트 코드
-print(branch_infos)
+wb.save('오렌지_보틀.xlsx')
