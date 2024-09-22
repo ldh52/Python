201be9fd6f43f31516595dd8d9de39e7b87f4240
Donghyun Lee <166485055+ldh52@users.noreply.github.com>
2024-09-23 07:18:28 +0900

    Update music_selenium.py

--- selenium/music_selenium.py
+++ selenium/music_selenium.py
@@ -3,7 +3,6 @@ from time import sleep
 
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
 
 
